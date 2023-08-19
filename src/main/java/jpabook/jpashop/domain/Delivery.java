264de3885f4e87704ee4e8c97f5320edd4509ebe
yurison <yu_ri_son@naver.com>
2023-08-20 08:46:57 +0900

    기본 엔티티 구현

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@DiscriminatorValue("A")
+@Entity
+public class Album extends Item {
+
+    private String artist;
+    private String etc;
+
+}
