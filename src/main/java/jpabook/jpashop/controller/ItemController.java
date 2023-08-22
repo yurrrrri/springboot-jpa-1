52b9865f156076b7cc12a2f82d16f7258517d50c
yurison <yu_ri_son@naver.com>
2023-08-23 08:22:10 +0900

    상품 목록 구현

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -35,4 +35,10 @@ public class ItemController {
         return "redirect:/items";
     }
 
+    @GetMapping("/items")
+    public String list(Model model) {
+        model.addAttribute("items", itemService.findItems());
+        return "items/itemList";
+    }
+
 }
