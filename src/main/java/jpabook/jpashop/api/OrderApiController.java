2217309b8a4e5c24b7b560b4f7a299af9f66d827
yurison <yu_ri_son@naver.com>
2023-10-01 15:27:18 +0900

    OrdersV3 완료

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -43,6 +43,7 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream().map(OrderDto::new).toList();
     }
 
     @Getter
